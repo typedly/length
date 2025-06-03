8e136e082b7897144013d9c039bb2ab6a3799a10
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-06-03 07:48:12 +0000

    test: update.

---
+++ src/test/length-configuration.spec.ts
@@ -0,0 +1,8 @@
+import { LengthConfiguration } from '../lib';
+
+// Length and pattern exact value.
+const lengthConfiguration1: LengthConfiguration<0, 27, 47> = {
+  value: 0,
+  min: 27,
+  max: 47,
+};
