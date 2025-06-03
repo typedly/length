7c7a2a2067be48860cd3f8405d9d5af887d03dab
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-06-03 07:37:57 +0000

    chore: move to the interface directory.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,1 @@
-
-export type { Length } from './length.interface';
-export type { LengthConfiguration } from './length-configuration.interface';
-export type { LengthOptions } from './length-options.interface';
-export type { LengthSetting } from './length-setting.interface';
-export type { LengthSettings } from './length-settings.interface';
+export * from './interface';
